fdf06847e97aed1993075e79da5313c0b562ca64
Abdelkarim Amghar <abdelkarimamghar@Abdelkarims-MacBook-Pro.local>
2023-10-20 20:10:16 +0200

    fix pagination

--- src/pages/movieSearch/movieSearch.js
+++ src/pages/movieSearch/movieSearch.js
@@ -59,7 +59,7 @@ const MovieSearch = () => {
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             onClick={() => setPageNumber(pageNumber - 1)}
-            disabled={pageNumber === 0}
+            disabled={pageNumber === 1}
           >
             <span className="">Prev</span>
           </button>
